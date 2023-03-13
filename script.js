af03c652445eb1adfcf60178e867cf2ed3240629
vanrley dev <99355912+hdbrapps@users.noreply.github.com>
2023-03-13 19:07:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
+// Seleciona o botão de menu e o menu lateral
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+// Adiciona um evento de clique ao botão de menu para alternar a classe 'open'
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('open');
+});
+
+// Seleciona o botão de alternar tema e a tag 'body'
+const toggleThemeBtn = document.querySelector('#toggle-theme');
+const body = document.querySelector('body');
+
+// Adiciona um evento de clique ao botão de alternar tema para alternar a classe 'dark'
+toggleThemeBtn.addEventListener('click', () => {
+  body.classList.toggle('dark');
+});
+
 // Selecione o botão de envio e adicione um evento de clique a ele
-document.getElementById("send").addEventListener("click", function(event) {
+document.getElementById('send').addEventListener('click', function(event) {
   // Evite o comportamento padrão de envio do formulário
   event.preventDefault();
-  
+
   // Selecione o valor do nome e da mensagem dos elementos de entrada
-  var nome = document.getElementById("nome").value;
-  var mensagem = document.getElementById("mensagem").value;
-  
+  var nome = document.getElementById('nome').value;
+  var mensagem = document.getElementById('mensagem').value;
+
   // Selecione o valor do serviço selecionado
-  var servico = document.getElementById("servico").value;
-  
+  var servico = document.getElementById('servico').value;
+
   // Construa a URL do WhatsApp com os valores do nome, mensagem e serviço
-  var url = "https://wa.me/559897018089?text=" + encodeURIComponent("Nome: " + nome + "\nServiço: " + servico + "\nMensagem: " + mensagem);
-  
+  var url = 'https://wa.me/559897018089?text=' + encodeURIComponent('Nome: ' + nome + '\nServiço: ' + servico + '\nMensagem: ' + mensagem);
+
   // Abra a URL do WhatsApp
   window.open(url);
-});
-
-// Selecione o botão do menu e adicione um evento de clique a ele
-document.getElementById("menu-toggle").addEventListener("click", function() {
-  // Selecione o elemento que contém o menu
-  var menu = document.getElementById("menu");
-  
-  // Adicione ou remova a classe "open" para mostrar ou esconder o menu
-  menu.classList.toggle("open");
-});
-
-// Selecione o botão de alternar tema e adicione um evento de clique a ele
-document.getElementById("theme-toggle").addEventListener("click", function() {
-  // Selecione o corpo da página
-  var body = document.body;
-  
-  // Adicione ou remova a classe "dark" para alternar o modo noturno
-  body.classList.toggle("dark");
 });
