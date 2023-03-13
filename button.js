81151cdead26dba5bd8b3e3c009c8e7afd85a1ef
vanrley dev <99355912+hdbrapps@users.noreply.github.com>
2023-03-13 19:08:34 -0300

    Update button.js

--- button.js
+++ button.js
@@ -1,16 +1,1 @@
-const slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
 
-function nextSlide() {
-  slides[currentSlide].classList.remove("active");
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].classList.add("active");
-}
-
-setInterval(nextSlide, 8000);
-
-// Remova o preloader após o carregamento da página
-window.addEventListener('load', function(){
-  var loader = document.getElementById('loading');
-  loader.style.display = 'none';
-});
