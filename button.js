08989b720e9d1452789e7150f457d30aee6b7d05
vanrley dev <99355912+hdbrapps@users.noreply.github.com>
2023-04-10 10:11:31 -0300

    commit in

--- button.js
+++ button.js
@@ -1,28 +1,5 @@
-const slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
-
-function nextSlide() {
-  slides[currentSlide].classList.remove("active");
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].classList.add("active");
-}
-
-setInterval(nextSlide, 8000);
-
-// Remova o preloader após o carregamento da página
-window.addEventListener('load', function(){
-  var loader = document.getElementById('loading');
-  loader.style.display = 'none';
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+      document.querySelector("body").classList.add("loaded");
+  }, 10)
 });
-
-var bar = new ProgressBar.Circle(progress, {
-  strokeWidth: 6,
-  easing: 'easeInOut',
-  duration: 1400,
-  color: '#FFEA82',
-  trailColor: '#eee',
-  trailWidth: 1,
-  svgStyle: null
-});
-
-bar.animate(0.2); // A porcentagem de progresso a ser exibida, neste caso 90%
